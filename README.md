2c22c67d70ba75603835b1398b8f1b74836375e7
Vinícius Mendonça <vinicius.mendonca@xa.epicgames.com>
2024-04-08 21:16:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Modular canvas
+
+Exibe uma bola animada utilizando o canvas do html 5 e a função `requestAnimationFrame`.
+
+* A pasta modules contém todos os scripts da página
+* Na pasta engine, está o código relacionado ao motor de jogo, ou seja, a infra-estrutura necessária para realizar 
+animação, fazer cálculos, etc. Este código se manteria o mesmo para qualquer jogo.
+* Na pasta game está o código dessa animação em si (da bola quicando).
---
+++ modules/game/bouncingBall.mjs
@@ -0,0 +1,37 @@
+import {toRad} from "../engine/math.mjs";
+import {animate} from "../engine/animation.mjs";
+
+const canvas = document.querySelector('#canvas');
+
+const RADIUS = 20;
+const SPEED = 100;
+
+let y = canvas.height / 2
+let velocity = SPEED;
+
+function update(time) {
+    //Deslocamento da bola
+    y += velocity * time;
+
+    // Inverte a direção da bola se atingir os cantos.
+    if (y < RADIUS) velocity = SPEED;
+    else if (y > canvas.width - RADIUS) velocity = -SPEED;
+}
+
+function draw(ctx) {
+    ctx.clearRect(0, 0, 400, 400);
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.fillStyle = "pink";
+    ctx.lineWidth = 2;
+
+    ctx.arc(200, y, RADIUS, 0, toRad(360));
+
+    ctx.fill();
+    ctx.stroke();
+}
+
+animate(
+    document.querySelector('#canvas'),
+    {update, draw}
+)
